b1ee65f9aa1ee819318b4f0e33fb3154196e2faa
Shahad161 <shahad.hamza.014@gmail.com>
2023-07-13 21:20:41 +0300

    finish the website

--- src/component/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Footer = () => {
-    return <Footer>
-       <h2>"footer"</h2>
-    </Footer>
-}
-
-export default Footer;
--- src/component/Main.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Main = () => {
-    return <main>
-       <h2>"main"</h2>
-    </main>
-}
-
-export default Main;
